4bf9ed1a82f76571ac6159e3a8ed6fd31241b9c9
SholayThakur <86546742+ShreyasSangai@users.noreply.github.com>
2026-05-04 23:12:11 +0530

    Simplify Jenkins installation script
    Removed unnecessary comments and streamlined commands.

--- jenkins-server.sh
+++ jenkins-server.sh
@@ -5,19 +5,16 @@
 # Jenkins Installation
 ################################################################################################
 
-sudo apt update -y  # It will update repo 
-sudo apt install fontconfig openjdk-21-jre # It will install Openjdk
-
-sudo java -version  # It will check java version
-
+sudo apt update
+sudo apt install fontconfig openjdk-21-jre -y
+java -version
 sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
   https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
 echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt update
-sudo apt install jenkins -y  # It will install jenkins
-echo "jenkins ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+sudo apt install jenkins -y
 
 #####################################################################################################
 # Terraform Installation
